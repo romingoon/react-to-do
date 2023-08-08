0a7a5481763d4cb2592fb675573cae2685c18f8d
romingoon <romingoon@gmail.com>
2023-08-08 16:24:30 +0900

    todos 배열에 새객체 추가 및 TodoInsert에서 onSubmit 이벤트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
@@ -18,9 +18,24 @@ const App = () => {
     { id: 3, text: '일정관리 앱  만들어보기', checked: false },
   ]);
 
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text: string) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextId 1씩 더하기
+    },
+    [todos]
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
