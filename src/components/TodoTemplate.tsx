2a87878d78e269af801bbd2f25ce52221efa60e7
romingoon <romingoon@gmail.com>
2023-08-08 15:37:05 +0900

    TodoTemplate 컴포넌트 만들기/ TodoTemplate 스타일링

---
+++ src/components/TodoTemplate.tsx
@@ -0,0 +1,12 @@
+import './TodoTemplate.scss';
+
+const TodoTemplate = ({ children }: any) => {
+  return (
+    <div className='TodoTemplate'>
+      <div className='app-title'> 할 일 관리</div>
+      <div className='content'>{children}</div>
+    </div>
+  );
+};
+
+export default TodoTemplate;
